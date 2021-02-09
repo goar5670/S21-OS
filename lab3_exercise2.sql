d70ec9bec8549d4ef64b818cb7905a839884ea33
Mahmoud Adel <40712841+goar5670@users.noreply.github.com>
2021-02-09 09:58:00 +0200

    Add files via upload

---
+++ lab3_exercise2.sql
@@ -0,0 +1,73 @@
+CREATE OR REPLACE VIEW public.strygin_view
+ AS
+ SELECT customer.customer_id,
+	customer.store_id,
+	customer.first_name,
+	customer.last_name,
+	customer.email,
+	customer.address_id,
+	customer.activebool,
+	customer.create_date,
+	customer.last_update,
+	customer.active
+   FROM customer
+  WHERE ((customer.last_name)::text ~~ 'S%'::text);
+
+ALTER TABLE public.strygin_view
+	OWNER TO dcnick3;
+
+-- View: public.nikita_view
+
+-- DROP VIEW public.nikita_view;
+
+CREATE OR REPLACE VIEW public.nikita_view
+ AS
+ SELECT customer.customer_id,
+	customer.store_id,
+	customer.first_name,
+	customer.last_name,
+	customer.email,
+	customer.address_id,
+	customer.activebool,
+	customer.create_date,
+	customer.last_update,
+	customer.active
+   FROM customer
+  WHERE ((customer.first_name)::text ~~ 'N%'::text);
+
+ALTER TABLE public.nikita_view
+	OWNER TO dcnick3;
+
+-- Trigger
+
+-- FUNCTION: public.remove_bad_guys()
+
+-- DROP FUNCTION public.remove_bad_guys();
+
+CREATE FUNCTION public.remove_bad_guys()
+	RETURNS SETOF trigger
+	LANGUAGE 'plpgsql'
+	COST 100
+	VOLATILE NOT LEAKPROOF
+	ROWS 1
+AS $BODY$
+BEGIN
+    IF NOT NEW.first_name LIKE 'Bad%' THEN
+   	 RETURN NEW;
+    END IF;
+    RAISE 'NO BAD GUYS ALLOWED!!';
+END;
+$BODY$;
+
+ALTER FUNCTION public.remove_bad_guys()
+
+-- Trigger: remove_bad_guys
+
+-- DROP TRIGGER remove_bad_guys ON public.customer;
+
+CREATE TRIGGER remove_bad_guys
+	BEFORE INSERT
+	ON public.customer
+	FOR EACH ROW
+	EXECUTE PROCEDURE public.remove_bad_guys();
+
