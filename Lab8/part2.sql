7a98d78e84236e860b7fda0c88f5e2695da5f3e6
KelDakroury <64041689+KelDakroury@users.noreply.github.com>
2021-03-23 09:51:04 +0200

    Update part2.sql

--- Lab8/part2.sql
+++ Lab8/part2.sql
@@ -1,23 +1,22 @@
-alter table accounts add bankName varchar(50);
+alter table accounts4 add bankName varchar(50);
 
-update accounts set bankName = 'SpearBank' where id = 1 or id = 3;
-update accounts set bankName = 'Tinkoff' where id = 2;
+update accounts4 set bankName = 'SpearBank' where id = 1 or id = 3;
+update accounts4 set bankName = 'Tinkoff' where id = 2;
 
-insert into accounts(id, name, credit) values(4, 'Fees', 0);
+insert into accounts4(id, name, credit) values(4, 'Fees', 0);
 
 begin;
   savepoint T0;
-  update accounts set credit = credit - 500 where id = 1;
-  update accounts set credit = credit + 500 where id = 3;
+  update accounts4 set credit = credit - 500 where id = 1;
+  update accounts4 set credit = credit + 500 where id = 3;
   savepoint T1;
-  update accounts set credit = credit - 700 where id = 2;
-  update accounts set credit = credit + 700 where id = 1;
-  update accounts set credit = credit + 30 where id = 4;
+  update accounts4 set credit = credit - 700 where id = 2;
+  update accounts4 set credit = credit + 670 where id = 1;
+  update accounts4 set credit = credit + 30 where id = 4;
   savepoint T2;
-  update accounts set credit = credit - 100 where id = 2;
-  update accounts set credit = credit + 100 where id = 3;
-  update accounts set credit = credit + 30 where id = 4;
+  update accounts4 set credit = credit - 100 where id = 2;
+  update accounts4 set credit = credit + 70 where id = 3;
+  update accounts4 set credit = credit + 30 where id = 4;
 commit;
 
-select id, credit from accounts;
-
+select id, credit from accounts4;
