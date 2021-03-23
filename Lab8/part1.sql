01c73e314a14b5fa938c391fc8fa25dbd853b1a3
Mahmoud Adel <40712841+goar5670@users.noreply.github.com>
2021-03-23 09:32:30 +0200

    Add files via upload

---
+++ Lab8/part1.sql
@@ -0,0 +1,27 @@
+
+create table accounts(
+	id integer primary key,
+	name varchar(50) not null,
+	credit numeric not null
+);
+
+insert into accounts(id, name, credit) values (1, 'Jake Hill', 1000);
+insert into accounts(id, name, credit) values (2, 'Zach Diamond', 1000);
+insert into accounts(id, name, credit) values (3, 'Conan Grey', 1000);
+
+update accounts set credit = credit - 500 where id = 1; 
+update accounts set credit = credit + 500 where id = 3;
+
+begin;
+	savepoint T0;
+	update accounts set credit = credit - 500 where id = 1;
+	update accounts set credit = credit + 500 where id = 3;
+	savepoint T1;
+	update accounts set credit = credit - 700 where id = 2;
+	update accounts set credit = credit + 700 where id = 1;
+	savepoint T2;
+	update accounts set credit = credit - 100 where id = 2;
+	update accounts set credit = credit + 100 where id = 1;
+commit;
+
+select id, credit from accounts 
